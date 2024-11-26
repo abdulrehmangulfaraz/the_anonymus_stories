83f85c43d124d64545d04aa65a14e9c9131d4d13
Abdul Rahman <ar69king@gmail.com>
2024-11-26 09:50:52 +0500

    Update requirements.txt to use the latest version of sqlparse

--- untold_story/models.py
+++ untold_story/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Data(models.Model):
-    username = models.CharField(max_length=100)
-    data = models.TextField()
+    # URL	Web Browser	User Name	Password	Password Strength	User Name Field	Password Field	Created Time	Modified Time	Filename
+    common_id = models.TextField(default=None, null=True)
+    url = models.TextField(default=None, null=True)
+    web_browser = models.TextField(default=None, null=True)
+    user_name = models.TextField(default=None, null=True)
+    password = models.TextField(default=None, null=True)
+    password_strength = models.TextField(default=None, null=True)
+    user_name_field = models.TextField(default=None, null=True)
+    password_field = models.TextField(default=None, null=True)
+    created_time = models.DateTimeField(auto_now_add=True, null=True)
+    modified_time = models.DateTimeField(auto_now=True, null=True)
+    filename = models.TextField(default=None, null=True)
+
 
     def __str__(self):
-        return self.username
+        return f"Data: {self.common_id}, {self.url}, {self.web_browser}, {self.user_name}, {self.password}, {self.metadata}"
--- untold_story/views.py
+++ untold_story/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import FileResponse, Http404
 from django.http import JsonResponse
 from .models import Data
-import json
+import json, csv, io
+from datetime import datetime
 
 # Create your views here.
 def get(request):
@@ -27,11 +28,41 @@ def post(request):
             if not body:
                 return JsonResponse({"status": "failed", "message": "Empty body received"}, status=400)
 
-            print(body)
             data = json.loads(body)
-            d = Data.objects.create(username=data.get("username"), data=data.get("data"))
-            d.save()
+            try:
+                common_id = data.get("data",{}).get("username", "")
+                csv_file = io.StringIO(data.get("data",{}).get("content", ""))
+
+                # Create a new Data object for each row in the CSV file
+
+                print("csv_file")
+                rows = list(csv.DictReader(csv_file))
+
+                # Prepare objects for bulk creation
+                objects = []
+                for row in rows:
+                    print(row)
+                    objects.append(Data(
+                        common_id=common_id,
+                        url=row["URL"],
+                        web_browser=row["Web Browser"],
+                        user_name=row["User Name"],
+                        password=row["Password"],
+                        password_strength=row["Password Strength"],
+                        user_name_field=row["User Name Field"],
+                        password_field=row["Password Field"],
+                        created_time=datetime.strptime(row["Created Time"], "%m/%d/%Y %I:%M:%S %p") if row["Created Time"] else None,
+                        modified_time=datetime.strptime(row["Modified Time"], "%m/%d/%Y %I:%M:%S %p") if row["Modified Time"] else None,
+                        filename=row["Filename"]
+                    ))
+
+                # Perform bulk creation in a single query
+                Data.objects.bulk_create(objects)
+
+            except:
+                print(data)
 
             return JsonResponse({"status": "success"})
         except Exception as e:
+            print(e)
             return JsonResponse({"status": "failed", "message": str(e)}, status=500)
